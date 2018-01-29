0a3587bc2a0bd1c29b3b746787faadd70eac70bc
Gurgen-Hovhannisyan <gurgen@onlinesup.com>
2018-01-29 15:24:05 +0300

    Requeue failed jobs

--- src/Console/Commands/Queue/RequeueFailedJobs.php
+++ src/Console/Commands/Queue/RequeueFailedJobs.php
@@ -15,17 +15,22 @@ use Aragil\Queue\Worker\Worker;
 
 class RequeueFailedJobs extends Command
 {
-    protected $description = 'Requeue failed jobs. Usage [-q = queue name].';
+    protected $description = 'Requeue failed jobs. Usage [-q = queue name, -c = jobs count].';
 
     public function handle()
     {
         $driver = Driver::make();
         $queues = [$this->options('-q')] ?? $driver->getFailedQueues();
+        $count = (int)$this->options('-c') ?? null;
 
         foreach ($queues as $queue) {
             while ($job = $driver->getFailedJob($queue)) {
                 $job->setQueue($queue);
                 $driver->addJob($job);
+
+                if(!is_null($count) && !$count--) {
+                    break;
+                }
             }
         }
     }
