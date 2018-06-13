8bd055c1fba2688bea5c1ca911f4c4b411e7f581
Gurgen-Hovhannisyan <gurgen@onlinesup.com>
2018-06-13 18:30:00 +0300

    Redis client

--- src/Storage/Redis.php
+++ src/Storage/Redis.php
@@ -8,12 +8,29 @@
 
 namespace Aragil\Storage;
 
-use Predis\Client;
-
 class Redis
 {
+    private static $connectionParams = [
+        'host'              =>  null,
+        'port'              =>  null,
+        'timeout'           =>  null,
+        'reserved'          =>  null,
+        'retry_interval'    =>  null,
+        'read_timeout'      =>  null
+    ];
+
     public static function client()
     {
-        return new Client(ini('redis'));
+        self::$connectionParams = array_merge(self::$connectionParams, ini('redis'));
+        $client = new \Redis();
+        $client->connect(
+            self::$connectionParams['host'] ?? 'localhost',
+            (int) self::$connectionParams['port'],
+            (int) self::$connectionParams['timeout'],
+            self::$connectionParams['reserved'] ?? '',
+            (int) self::$connectionParams['retry_interval'],
+            (int) self::$connectionParams['read_timeout']
+        );
+        return $client;
     }
 }
